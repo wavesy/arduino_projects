07de33a56405d45626851a9d08b565fc3b0d32a1
Saku <saku.aaltonen12@gmail.com>
2023-01-31 23:24:56 +0200

    temp, humidity prints on OLED I2C screen

--- air_quality/air_quality.ino
+++ air_quality/air_quality.ino
@@ -1,19 +1,64 @@
-// TODO: add screen, mq-135 later
+// TODO: add mq-135 later, reading updates only with change
+// TODO: figure out setCursor
 
 #include <DHT.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 #define DHTPIN 5        // temp/humidity sensor
 #define DHTTYPE DHT11
 #define SAMPLERATE 2000 // sampling rate in ms
+#define OLEDADDRESS 0x3C
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
 
-float tempoffset = 0.0;
-float humidityoffset = 0.0;
+// fine tuning variables
+const float tempoffset = 0.0;
+const float humidityoffset = 0.0;
 
+// declaration for temp/humidity sensor object
 DHT dht(DHTPIN, DHTTYPE);
 
+// declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+
 void setup() {
-  dht.begin();
+  // init serial monitor
   Serial.begin(9600);
+
+  // init temp / humidity sensor
+  dht.begin();
+  
+  // init display
+  if(!display.begin(SSD1306_SWITCHCAPVCC, OLEDADDRESS)) { 
+    Serial.println(F("SSD1306 allocation failed"));
+    while(1); // stay here
+  }
+  display.clearDisplay();
+  display.setTextSize(3);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  display.print("Setting up");
+  delayMillis(500);
+  display.print(".");
+  display.display();
+  delayMillis(500);
+  display.print(".");
+  display.display();
+  delayMillis(500);
+  display.print(".");
+  display.display();
+
+  display.clearDisplay();
+  /*
+  display.setCursor(0,5);
+  display.print("C");
+  display.setCursor(32,5);
+  display.print("RH%");
+  */
+  display.display();
 }
 
 void delayMillis(unsigned long t){
@@ -28,11 +73,22 @@ void loop() {
   // dht sensor reading ~250 ms
   float humidity = dht.readHumidity() + humidityoffset;
   float temp = dht.readTemperature() + tempoffset;
+  
+  // debug
+  Serial.print("T: ");
+  Serial.println(temp);
+  delayMillis(2000);
+  //
+  display.setCursor(0,0);
+  display.println(temp);
+  display.display();
+
   // debug
   Serial.println();
   Serial.print("H: ");
   Serial.println(humidity);
-  Serial.print("T: ");
-  Serial.println(temp);
-  delayMillis(2000);
+  //
+  display.setCursor(0,32);
+  display.println(humidity);
+  display.display();
 }
